d86e4f84acf8ac5641c5bfcae9e14c8e3e8f304e
eldisnex <ulest20@gmail.com>
2025-03-10 15:55:41 -0300

    Sin css

---
+++ index.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('form');
+const promedio = document.getElementById('promedio');
+const mayor = document.getElementById('mayor');
+const resultado = document.getElementById('resultado');
+const inputs = Array.from(document.querySelectorAll('input'));
+
+form.addEventListener('submit', (e) => e.preventDefault());
+
+promedio.addEventListener(
+   'click',
+   () =>
+      (resultado.textContent =
+         inputs.reduce((a, b) => a + parseFloat(b.value), 0) / inputs.length)
+);
+mayor.addEventListener(
+   'click',
+   () =>
+      (resultado.textContent = Math.max(
+         ...inputs.map((e) => parseFloat(e.value))
+      ))
+);
